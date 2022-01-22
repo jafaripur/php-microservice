13a166679cdedae40653e4921cbb19795b0038b6
Araz <mjafaripur@yahoo.com>
2022-01-22 20:04:31 +0330

    Add code style fix, php lint to find error. Add bash script ./lint to run all requirement before commit and pus.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,26 @@
+<?php 
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude([
+        'vendor',
+        '.git',
+        'tools',
+        'temp',
+        '.github',
+        'docker',
+    ])
+    ->in(__DIR__ . "/../../");
+
+$config = new PhpCsFixer\Config();
+
+$config->setRules([
+        '@PSR12' => true,
+        '@PHP80Migration' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setRiskyAllowed(true)
+    ->setCacheFile(__DIR__.'/.php-cs-fixer.cache')
+    ->setFinder($finder);
+
+return $config;
